a0a17fd2b1a3384e60829253ad601fa37bb0e810
gauravsoni97 <gauravsoni8414@gmail.com>
2026-03-31 17:41:16 +0530

    feat: initialize job board project with React, Flowbite, and Tailwind CSS
    - Added main application structure with App component.
    - Implemented AddUserForm component for user registration.
    - Included SVG icons for various functionalities.
    - Integrated CSS styles using Tailwind and Flowbite.
    - Configured Vite for development with React and Tailwind CSS.
    - Added assets including images and SVGs for branding.

---
+++ ReactJs/job_board/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AddUserForm from "./components/AddUserForm";
+
+const App = () => {
+  return (
+    <div style={{ marginTop: "1rem" }}>
+    <AddUserForm />
+    </div>
+  );
+};
+
+export default App;
--- ReactJs/job_board/src/main.jsx
+++ ReactJs/job_board/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ThemeInit } from '../.flowbite-react/init.tsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ThemeInit />
     <App />
   </StrictMode>,
 )
